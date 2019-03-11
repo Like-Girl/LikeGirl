8bac3a7d3d4aecec2fc7ac081196e50ab20a392c
ke.chen <likegirl.cn@gmail.com>
2019-03-11 14:15:10 +0800

    時間

---
+++ LikeGirl-Common/src/main/java/cn/likegirl/common/core/support/file/excel/IRowReader.java
@@ -0,0 +1,14 @@
+package cn.likegirl.common.core.support.file.excel;
+
+import java.util.List;
+
+public interface IRowReader {
+	/**
+	 * 业务逻辑实现方法
+	 * 
+	 * @param sheetIndex
+	 * @param curRow
+	 * @param rowlist
+	 */
+	void getRows(int sheetIndex, int curRow, List<String> rowlist);
+}
---
+++ LikeGirl-Web/src/main/java/cn/likegirl/web/App.java
@@ -0,0 +1,13 @@
+package cn.likegirl.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
